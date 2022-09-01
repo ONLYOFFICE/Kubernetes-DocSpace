9fb89047a63ce66108572ce0e48fc60208feed1e
Vyacheslav Semin <sla1733@yandex.ru>
2022-09-01 10:31:26 +0300

    Add docspace services (#12)
    * Add docspace services
    
    * Change statefulsets of new services
    
    * Change comtainer names of new services
    
    * Fix variable names and upstream (#13)
    
    * Fix proxy upstream and job install
    
    * Add the ability to install appserver (#14)
    
    * Add the ability to install appserver
    
    * Fix job update
    
    * Add resources to manifests
    
    * Add resources to initContainer
    
    * Change the application name (#15)
    
    Co-authored-by: kireevdmitry <octavarium78@gmail.com>

--- sources/scripts/update-app.sh
+++ sources/scripts/update-app.sh
@@ -10,25 +10,25 @@ else
   APPSERVER_VERSION=$1
 fi
 
-init_appserver_update_job(){
-  kubectl get job | grep -iq appserver-update
+init_app_update_job(){
+  kubectl get job | grep -iq app-update
   if [ $? -eq 0 ]; then
-    echo -e "\e[0;31m A Job named appserver-update exists. Exit  \e[0m"
+    echo -e "\e[0;31m A Job named app-update exists. Exit  \e[0m"
     exit 1
   else
     # wget https://raw.githubusercontent.com/ONLYOFFICE/server/master/schema/mysql/changedb.sql
     # kubectl create configmap change-db-scripts --from-file=./changedb.sql
-    kubectl apply -f ./templates/configmaps/update-appserver.yaml
+    kubectl apply -f ./templates/configmaps/update-app.yaml
   fi
 }
 
-create_appserver_update_job(){
-  kubectl apply -f ./templates/jobs/appserver-update.yaml
+create_app_update_job(){
+  kubectl apply -f ./templates/jobs/app-update.yaml
   sleep 5
-  PODNAME="$(kubectl get pod | grep -i appserver-update | awk '{print $1}')"
+  PODNAME="$(kubectl get pod | grep -i app-update | awk '{print $1}')"
 }
 
-check_appserver_update_pod_status(){
+check_app_update_pod_status(){
   while true; do
       STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
       case $STATUS in
@@ -49,37 +49,37 @@ check_appserver_update_pod_status(){
   done
 }
 
-delete_appserver_update_job(){
-  echo -e "\e[0;32m Status of the appserver-update POD: $POD_STATUS. The Job will be deleted. \e[0m"
-  kubectl delete job appserver-update
+delete_app_update_job(){
+  echo -e "\e[0;32m Status of the app-update POD: $POD_STATUS. The Job will be deleted. \e[0m"
+  kubectl delete job app-update
 }
 
 update_images(){
   for sts in "${APPSERVER_STS[@]}"; do
      kubectl set image statefulset/$sts \
-       $sts=onlyoffice/4testing-appserver-$(echo $sts | sed 's/[^-]*-//'):${APPSERVER_VERSION}
+       $sts=onlyoffice/4testing-app-$(echo $sts | sed 's/[^-]*-//'):${APPSERVER_VERSION}
   done
 
   for deployment in "${APPSERVER_DEPLOYMENT[@]}"; do
     kubectl set image deployment/$deployment \
-      $deployment=onlyoffice/4testing-appserver-$(echo $deployment | sed 's/[^-]*-//'):${APPSERVER_VERSION}
+      $deployment=onlyoffice/4testing-app-$(echo $deployment | sed 's/[^-]*-//'):${APPSERVER_VERSION}
   done
 }
 
 print_error_message(){
-  echo -e "\e[0;31m Status of the appserver-update POD: $POD_STATUS \e[0m"
+  echo -e "\e[0;31m Status of the app-update POD: $POD_STATUS \e[0m"
   echo -e "\e[0;31m The Job will not be deleted automatically. Further actions to manage the Job must be performed manually. \e[0m"
 }
 
-init_appserver_update_job
-create_appserver_update_job
+init_app_update_job
+create_app_update_job
 
-echo "Getting the appserver-update POD status..."
-POD_STATUS=$(check_appserver_update_pod_status)
+echo "Getting the app-update POD status..."
+POD_STATUS=$(check_app_update_pod_status)
 if [[ "$POD_STATUS" == "error" ]]; then
   print_error_message
 else
-  delete_appserver_update_job
+  delete_app_update_job
   update_images
   echo -e "\e[0;32m The Job update was completed successfully. Wait until all containers with the new version of the images have the READY status. \e[0m"
 fi
